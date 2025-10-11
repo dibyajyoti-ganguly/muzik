4d0189f3e3beb851fb6b0264339288d29b21b5ad
Dibyajyoti Ganguly <gangulyd49@gmail.com>
2025-10-11 11:28:40 +0530

    Use session provider to track if/when user's session is active

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,33 @@
 "use client";
 
-import { signIn } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
-export default function Home() {
+export default function Appbar() {
+  const session = useSession();
+
   return (
     <div className="flex justify-around mt-4">
       <p>Hello Next!</p>
-      <button
-        className="w-24 h-10 bg-red-600 text-white cursor-pointer"
-        onClick={() => {
-          signIn();
-        }}
-      >
-        Signin
-      </button>
+      {session.data?.user && (
+        <button
+          className="w-24 h-10 bg-blue-600 text-white cursor-pointer"
+          onClick={() => {
+            signOut();
+          }}
+        >
+          Logout
+        </button>
+      )}
+      {!session.data?.user && (
+        <button
+          className="w-24 h-10 bg-red-600 text-white cursor-pointer"
+          onClick={() => {
+            signIn();
+          }}
+        >
+          Sign In
+        </button>
+      )}
     </div>
   );
 }
